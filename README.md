69cdf596614d59dcea8a455dbf337a06bb0cbea0
israncho <isragutierrez@ciencias.unam.mx>
2023-05-08 02:31:58 -0600

    first try

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# greedy-set-cover
-Implementation of an approximation algorithm for the NP-Complete problem Set Cover.
+# Greedy Set Cover
+
+Algoritmo de aproximacion para el problema NP-completo SET COVER.
+
+## Set Cover 
+
+## Decision
+
+**EJEMPLAR:** Un conjunto $U$, una coleccion de subconjuntos $C=\{S_0,S_1,...,S_m\}$ 
+y un entero $k$ tal que
+$$\bigcup^m_{i = 1} S_i = U$$
+
+
+**PREGUNTA:** ¿Existe $C'\subseteq C$ con $|C'|\leq k$ tal que 
+cada elemento de $U$ pertenece al menos a un miembro de $C'$?
+
+## Optimizacion 
+
+**EJEMPLAR:** Un conjunto $U$, una coleccion de subconjuntos $C=\{S_0,S_1,...,S_m\}$ 
+tal que
+$$\bigcup^m_{i = 1} S_i = U$$
+
+**SALIDA:** $C'\subseteq C$ tal que cada elemento de $U$ pertenece al menos a un miembro de $C'$
+y $|C'|$ es la menor posible.
+
+
+
+
+ 
+
+
+
+#### Jesus Israel Gutierrez Elizalde
---
+++ src/greedy_set_cover_lists.py
@@ -0,0 +1,42 @@
+def diff(l1: list, l2: list) -> None:
+    '''Computes the difference between two lists `l1` and 
+    `l2` by performing the operation `l1`-`l2` on the list `l1`.'''
+    for elem in l2:
+        l1.remove(elem)
+
+
+def elements_covered(set1: list, set2: list) -> int:
+    '''Calculates the number of elements in `set2` that 
+    are contained in `set1`, or in other words, it calculates 
+    the number of elements that `set2` covers in `set1`.'''
+    covered_elems = 0
+    for e in set2:
+        if e in set1:
+            covered_elems = covered_elems + 1
+    return covered_elems
+
+
+def maximize_uncovered(uncovered_elems: list, subsets: list) -> int:
+    '''Returns the index of the subset in the given list 
+    `subsets` that covers the maximum number of
+    elements from the list `uncovered_elems`.'''
+    i_max = 0
+    max_covered_elems = 0
+    for i in range(len(subsets)):
+        s_i_covered = elements_covered(uncovered_elems, subsets[i])
+        if s_i_covered > max_covered_elems:
+            i_max = i
+    return i_max
+
+
+def greedy_set_cover(universe: list, subsets: list) -> list:
+    '''Assuming the input instance is correct, it returns 
+    a list with the indices of the subsets that form a 
+    cover for the universe.'''
+    uncovered_elems = list(universe)
+    indexes = []
+    while uncovered_elems != []:
+        i_max = maximize_uncovered(uncovered_elems, subsets)
+        indexes.append(i_max)
+        diff(uncovered_elems, subsets[i_max])
+    return indexes
