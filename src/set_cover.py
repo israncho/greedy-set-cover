f15ef4c225c658e180229e0c87310b886eb59122
israncho <isragutierrez@ciencias.unam.mx>
2023-05-08 22:34:29 -0600

    funciones utilies

--- src/set_cover.py
+++ src/set_cover.py
@@ -16,9 +16,9 @@ def create_collection(universe: list) -> list:
     return subsets
 
 
-def has_solution(universe: list, collection: list) -> bool:
+def has_cover(universe: list, collection: list) -> bool:
     '''Returns true if exists a cover in `collection` for 
-    the `universe`'''
+    the `universe`.'''
     aux_set = set()
     for subset in collection:
         for element in subset:
@@ -26,7 +26,32 @@ def has_solution(universe: list, collection: list) -> bool:
     return aux_set == set(universe)
 
 
+def correct_instance(universe: list, collection: list) -> bool:
+    '''Returns true it the given instance of the SC problem
+    is correct.'''
+    if len(universe) != len(set(universe)):
+        return False
+    for subset in collection:
+        if len(subset) != len(set(subset)):
+            return False
+    return has_cover(universe, collection)
+
+
+def print_instance(universe: list, collection: list) -> None:
+    '''Prints in console the given instance of 
+    the set cover problem.'''
+    print('U = ', end='')
+    print(universe)
+    print('C = \n[' + str(collection[0]) + ',')
+    for i in range(1, len(collection)):
+        print(str(collection[i]), end='')
+        if i == len(collection) - 1:
+            print(']')
+        else:
+            print(',')
+
+
 u = list(range(30))
 c = create_collection(u)
-print(c)
-print(has_solution(u, c))
+print_instance(u, c)
+print(has_cover(u, c))
